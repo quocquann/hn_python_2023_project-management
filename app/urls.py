ab3b96a0b0ebcb746623d8e3d74c3561b0d2e536
Mar <nvmanh25101@gmail.com>
2023-12-05 16:30:19 +0700

    Log out

--- app/urls.py
+++ app/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
         auth_views.LoginView.as_view(template_name="registration/signin.html"),
         name="login",
     ),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
