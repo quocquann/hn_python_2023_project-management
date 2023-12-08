fcddd6ef3999ea0cd1b8699e4963b75fad130dd6
manhnv-1320 <nguyen.van.manh-e@sun-asterisk.com>
2023-12-08 13:48:51 +0700

    Add user to stage

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -3,7 +3,7 @@ from ..models import UserProject, UserStage
 
 
 def is_in_group(user):
-    return user.groups.filter(name__in=['Stage_Owner', 'PM']).exists()
+    return user.groups.filter(name__in=["Stage_Owner", "PM"]).exists()
 
 
 def check_token(user, token):
@@ -25,4 +25,10 @@ def is_stage_owner(user, stage):
 
 
 def is_stage_member_or_pm(user, stage):
-    return UserStage.objects.filter(user=user, stage=stage).exists() or is_pm(user, stage.project)
+    return UserStage.objects.filter(user=user, stage=stage).exists() or is_pm(
+        user, stage.project
+    )
+
+
+def is_pm_or_stage_owner(user, stage, project):
+    return is_pm(user, project) or is_stage_owner(user, stage)
