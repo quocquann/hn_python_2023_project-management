15cb4aef59ae05dabcc0263d213d7a0a069b07ce
manhnv-1320 <nguyen.van.manh-e@sun-asterisk.com>
2023-12-11 15:09:23 +0700

    start api app

--- projectmanagement/urls.py
+++ projectmanagement/urls.py
@@ -14,15 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.i18n import i18n_patterns
 from django.views.generic.base import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = i18n_patterns(
     path("admin/", admin.site.urls),
     path("", RedirectView.as_view(url="project/")),
     path("project/", include("app.urls")),
+    path("api/", include("api.urls")),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
