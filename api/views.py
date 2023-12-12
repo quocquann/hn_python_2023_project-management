110542ce3ff9b34fa221f0b83101d34438ef68c0
manhnv-1320 <nguyen.van.manh-e@sun-asterisk.com>
2023-12-12 13:22:43 +0700

    API Sign up

--- api/views.py
+++ api/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db import transaction
+from django.forms import model_to_dict
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
+from django.utils.translation import gettext_lazy as _
+from drf_spectacular.utils import extend_schema
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from app.utils.helpers import send_mail_verification
+from .serializers import SignUpSerializers, VerifySerializers
+
+
+class SignUp(APIView):
+    permission_classes = [AllowAny]
+
+    @extend_schema(
+        request=SignUpSerializers,
+        responses={
+            201: {"description": "Please check your email to verify your account."},
+        },
+    )
+    def post(self, request):
+        serializer = SignUpSerializers(data=request.data)
+        if serializer.is_valid():
+            with transaction.atomic():
+                serializer.save()
+                new_user = serializer.instance
+                send_mail_verification(request, new_user=new_user)
+                data = {
+                    "message": _("Please check your email to verify your account."),
+                    "user": serializer.data,
+                }
+
+                return Response(data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Verify(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, uidb64, token):
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+        user_dict = model_to_dict(
+            user, ["pk", "username", "email", "first_name", "last_name"]
+        )
+        data = {
+            "pk": uid,
+            "verify_token": token,
+        }
+        serializer = VerifySerializers(user, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                "message": _(
+                    "Thank you for your email confirmation. Now you can login your account."
+                ),
+                "user": user_dict,
+            }
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,5 +1,13 @@
+from uuid import uuid4
+
+from django.core.mail import send_mail
+from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
+from projectmanagement.settings import EMAIL_HOST_USER
 from . import constants
-from ..models import UserProject, UserStage
+from ..models import UserProject, UserStage, CustomUser
 
 
 def is_in_group(user):
@@ -34,3 +42,28 @@ def is_stage_member_or_pm(user, stage):
 
 def is_pm_or_stage_owner(user, stage, project):
     return is_pm(user, project) or is_stage_owner(user, stage)
+
+
+def send_mail_verification(request, new_user):
+    verify_token = uuid4()
+    CustomUser.objects.create(user=new_user, verify_token=verify_token)
+    mail_subject = "Activate your account."
+    verify_url = reverse(
+        "verify",
+        kwargs={
+            "uidb64": urlsafe_base64_encode(force_bytes(new_user.pk)),
+            "token": str(verify_token),
+        },
+    )
+    mail_message = (
+        f"Hi {new_user.username}, Please use this link to verify your account\n"
+        f"{request.build_absolute_uri(verify_url)}"
+    )
+    from_email = EMAIL_HOST_USER
+    send_mail(
+        mail_subject,
+        mail_message,
+        from_email,
+        [new_user.email],
+        fail_silently=False,
+    )
