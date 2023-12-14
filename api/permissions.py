22c3e625cf71948af87f05253231c0c69ecd4823
manhnv-1320 <nguyen.van.manh-e@sun-asterisk.com>
2023-12-14 09:21:47 +0700

    API stage list

--- api/permissions.py
+++ api/permissions.py
@@ -1,9 +1,19 @@
 from rest_framework import permissions
 
-from app.utils.helpers import is_pm
+from app.utils.helpers import is_pm, is_in_project
 
 
 class IsPM(permissions.BasePermission):
     def has_permission(self, request, view):
         project = view.kwargs.get("project_id")
         return is_pm(request.user, project)
+
+
+class IsPMOrProjectMember(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            project = view.kwargs.get("project_id")
+            return is_in_project(request.user, project)
+        else:
+            project = view.kwargs.get("project_id")
+            return is_pm(request.user, project)
