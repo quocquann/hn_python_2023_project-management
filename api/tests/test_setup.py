bc75fe16e93244405a54e5223e86540308f56d0b
manhnv-1320 <nguyen.van.manh-e@sun-asterisk.com>
2023-12-18 17:11:17 +0700

    Test signup view

--- api/tests/test_setup.py
+++ api/tests/test_setup.py
@@ -11,11 +11,19 @@ class TestSetUp(APITestCase, URLPatternsTestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = cls.setup_user()
+        cls.signup_data = {
+            "username": "user2",
+            "email": "user2@gmail.com",
+            "password1": "user2abcqwe",
+            "password2": "user2abcqwe",
+            "first_name": "2",
+            "last_name": "user",
+        }
 
     @staticmethod
     def setup_user():
         User = get_user_model()
 
         return User.objects.create_user(
-            username="user1", password="user1", email="user1@mail.com"
+            username="user1", password="user1", email="user1@gmail.com"
         )
--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+from rest_framework import status
 
 from api.tests.test_setup import TestSetUp
 
@@ -11,3 +12,107 @@ class LoginTest(TestSetUp):
         )
 
         self.assertEqual(response.status_code, 200)
+
+
+class SignUpTest(TestSetUp):
+    def test_signup(self):
+        response = self.client.post(
+            path=reverse("signup"), data=self.signup_data, format="json"
+        )
+        res_data = response.data.get("user")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(
+            response.data.get("message"),
+            "Please check your email to verify your account.",
+        )
+        self.assertEqual(res_data["username"], self.signup_data["username"])
+        self.assertEqual(res_data["email"], self.signup_data["email"])
+        self.assertEqual(res_data["first_name"], self.signup_data["first_name"])
+        self.assertEqual(res_data["last_name"], self.signup_data["last_name"])
+
+    def test_username_blank(self):
+        self.signup_data["username"] = ""
+        response = self.client.post(
+            path=reverse("signup"), data=self.signup_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["username"][0], "This field may not be blank.")
+
+    def test_username_exists(self):
+        self.signup_data["username"] = "user1"
+        response = self.client.post(
+            path=reverse("signup"), data=self.signup_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["username"][0], "This field must be unique.")
+
+    def test_email_blank(self):
+        self.signup_data["email"] = ""
+        response = self.client.post(
+            path=reverse("signup"), data=self.signup_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["email"][0], "This field may not be blank.")
+
+    def test_email_exists(self):
+        self.signup_data["email"] = "user1@gmail.com"
+        response = self.client.post(
+            path=reverse("signup"), data=self.signup_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["email"][0], "This field must be unique.")
+
+    def test_email_invalid(self):
+        self.signup_data["email"] = "user2"
+        response = self.client.post(
+            path=reverse("signup"), data=self.signup_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["email"][0], "Enter a valid email address.")
+
+    def test_password1_blank(self):
+        self.signup_data["password1"] = ""
+        response = self.client.post(
+            path=reverse("signup"), data=self.signup_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["password1"][0], "This field may not be blank.")
+
+    def test_password1(self):
+        self.signup_data["password1"] = "aaa"
+        response = self.client.post(
+            path=reverse("signup"), data=self.signup_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["password1"],
+            [
+                "This password is too short. It must contain at least 8 characters.",
+                "This password is too common.",
+            ],
+        )
+
+    def test_password1_common(self):
+        self.signup_data["password1"] = "aaaaaaaa"
+        response = self.client.post(
+            path=reverse("signup"), data=self.signup_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["password1"][0], "This password is too common.")
+
+    def test_password2_blank(self):
+        self.signup_data["password2"] = ""
+        response = self.client.post(
+            path=reverse("signup"), data=self.signup_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["password2"][0], "This field may not be blank.")
+
+    def test_password_not_match(self):
+        self.signup_data["password2"] = "aaa"
+        response = self.client.post(
+            path=reverse("signup"), data=self.signup_data, format="json"
+        )
+        self.assertEqual(
+            response.data["non_field_errors"][0], "Password does not match"
+        )
